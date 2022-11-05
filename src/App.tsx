1e0272901df5ba07d054cf6d1e171637088df7e8
a1tsv <forakk01@yandex.ru>
2022-11-05 23:49:56 +0300

    technologies component added

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          css
+        </li>
+        <li>
+          html
+        </li>
+        <li>
+          js
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Messages</a></li>
+        <li><a href="#">Home</a></li>
+      </ul>
+    </div>
+  )
+}
+
 
 export default App;
